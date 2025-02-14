e645776515b70c4a5880a93cb9f035e07ddfec57
rajakkamesh <rajakkamesh@gmail.com>
2025-02-15 00:47:53 +0530

    Update test.js

--- test/test.js
+++ test/test.js
@@ -1,9 +1,12 @@
-const { embedOleObject } = require("../index");
+const { embedFloatingOleObjects } = require("../index");
 
-// Test the embedding function
-const oleFile = "test/sample.xls"; // Replace with actual file path
+const oleFiles = [
+    { filePath: "test/sample.xls", customIconPath: "test/custom_excel_icon.png", width: 120, height: 120, x: 100, y: 200 },
+    { filePath: "test/report.pdf", width: 80, height: 80, x: 300, y: 150 }, // Uses default PDF icon
+];
+
 const outputDocx = "test/output.docx";
 
-embedOleObject(oleFile, outputDocx)
-  .then(() => console.log("OLE object embedded successfully!"))
-  .catch((error) => console.error("Error:", error));
+embedFloatingOleObjects(oleFiles, outputDocx)
+    .then(() => console.log("Floating OLE objects embedded successfully!"))
+    .catch((error) => console.error("Error:", error));
